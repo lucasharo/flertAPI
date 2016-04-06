6153bcd308716899c95e0ccae11f589b35a36c7d
Lucas Miranda <lucas-haro@hotmail.com>
2016-04-06 18:27:08 -0300

    Estruturação

---
+++ FlertAPI/app.js
@@ -0,0 +1,24 @@
+﻿var app = require('express')(),
+http = require('http').Server(app),
+io = require('socket.io')(http),
+chatCtrl = require('./controller/chatController')(io);
+
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+    chatCtrl.novoUsuario(socket);
+    
+    socket.on('login', function (login) {
+       chatCtrl.login(socket, login);
+    });
+
+    socket.on('mensagem', function (mensagem) {
+        chatCtrl.mensagem(socket, mensagem);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *: 3000');
+});
---
+++ FlertAPI/controller/chatController.js
@@ -0,0 +1,27 @@
+﻿module.exports = function (io) {
+    var sockets = [];      
+
+    module.novoUsuario = function (socket){
+        sockets.push(socket);
+        
+        console.log('connection');
+        
+        socket.emit('connect');
+    }
+    
+    module.login = function (socket, login) {
+        for (var i = 0; i < sockets.length; i++) {
+            if (sockets[i].id === socket.id) {
+                sockets[i].login = login;
+                console.log("Login");
+            }
+        }
+    }
+    
+    module.mensagem = function (socket, mensagem) {
+        socket.broadcast.emit('mensagem', { nickname: socket.login, mensagem: mensagem });
+    }
+
+    return module;
+}
+
